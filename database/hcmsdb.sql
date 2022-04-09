b6dc49a26033f732cd1d9f17ad96b99d374053fb
prasoonsaini <prasoonsaini2002@gmail.com>
2022-04-09 18:12:40 +0530

    user appointment and lab tables updated

--- database/hcmsdb.sql
+++ database/hcmsdb.sql
@@ -145,23 +145,11 @@ CREATE TABLE `lab_results` (
 -- Dumping data for table `lab_results`
 --
 
-INSERT INTO `lab_results` (`id`, `patient_id`, `malaria`, `typhoid`, `HIV/AIDs`, `blood_group`, `UTI`, `UTP`, `blood_pressure`, `weight`, `height`, `allergy`, `checked_by`, `status`) VALUES
+INSERT INTO `lab_results` (`id`, `patient_id`, `blood_group`, `UTI`, `UTP`, `blood_pressure`, `weight`, `height`, `allergy`, `checked_by`, `status`) VALUES
 (1, 2, 'No', 'No', 'No', 'A', 'No', 'No', 'No', 67, 59, 'No', 'Fatma ALi', 1),
 (2, 24, 'Yes', 'Yes', 'No', 'B', 'Yes', 'No', 'Yes', 50, 70, 'No', 'Mkarafuu Mkavu', 0),
 (4, 29, 'No', 'No', 'No', 'A', 'No', 'No', 'No', 50, 50, 'No', 'Mkarafuu Mkavu', 1),
-(5, 2, 'Yes', 'No', 'No', '0', 'No', 'Yes', 'No', 67, 70, 'No', 'John  Doe', 1),
-(6, 30, 'Yes', 'No', 'Positive', '0', 'Yes', 'Yes', 'No', 80, 90, 'No', 'John  Doe', 1),
-(7, 31, 'Yes', 'No', 'No', '0', 'Yes', 'No', 'Yes', 78, 77, 'Yes', 'Mkarafuu Mkavu', 1),
-(8, 33, 'Yes', 'Yes', 'No', 'B', 'Yes', 'No', 'No', 50, 50, 'No', 'Mkarafuu Mkavu', 1),
-(9, 34, 'Yes', 'No', 'No', 'B', 'No', 'No', 'No', 67, 78, 'No', 'Mkarafuu Mkavu', 1),
-(10, 35, 'No', 'No', 'No', 'B', 'No', 'No', 'No', 76, 78, 'No', 'Mkarafuu Mkavu', 1),
-(11, 36, 'Yes', 'No', 'Negative', 'A', 'No', 'No', 'No', 90, 50, 'No', 'Mkarafuu Mkavu', 1),
-(12, 34, 'Yes', 'No', 'Negative', 'n/a', 'n/a', 'n/a', 'n/a', 0, 0, 'n/a', 'Mkarafuu Mkavu', 1),
-(13, 32, 'Yes', 'n/a', 'n/a', 'n/a', 'No', 'n/a', 'No', 0, 0, 'n/a', 'John  Doe', 1),
-(14, 37, 'No', 'n/a', 'n/a', 'n/a', 'n/a', 'n/a', 'No', 0, 0, 'n/a', 'Mkarafuu Mkavu', 1),
-(15, 39, 'No', 'n/a', 'n/a', 'B', 'n/a', 'n/a', 'n/a', 69, 0, 'n/a', 'Mkarafuu Mkavu', 1),
-(16, 40, 'No', 'No', 'n/a', 'B', 'n/a', 'n/a', 'n/a', 0, 0, 'n/a', 'Mkarafuu Mkavu', 1),
-(17, 41, 'No', 'n/a', 'n/a', 'n/a', 'n/a', 'n/a', 'n/a', 0, 0, 'n/a', 'John  Doe', 1);
+
 
 -- --------------------------------------------------------
 
@@ -176,130 +164,7 @@ CREATE TABLE `malipo` (
   `status` varchar(30) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `malipo`
---
 
-INSERT INTO `malipo` (`malipo_id`, `patient_id`, `amount`, `status`) VALUES
-(9, 34, 201600, 'Paid'),
-(8, 30, 61000, 'Paid'),
-(7, 29, 3000, 'Paid'),
-(10, 30, 45750, 'Paid'),
-(11, 35, 61800, 'Paid'),
-(12, 2, 0, 'Paid'),
-(13, 34, 151200, 'Paid'),
-(14, 34, 151200, 'Paid'),
-(15, 36, 3600, 'Paid'),
-(16, 34, 375, 'Paid'),
-(17, 34, 375, 'Paid'),
-(18, 34, 375, 'Paid'),
-(19, 32, 2213, 'Paid'),
-(20, 32, 2213, 'Paid'),
-(21, 32, 2213, 'Paid'),
-(22, 37, 1500, 'Paid'),
-(23, 39, 900, 'Paid'),
-(24, 40, 159563, 'Paid'),
-(25, 41, 4875, 'Paid');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `medication`
---
-
-CREATE TABLE `medication` (
-  `id` int(10) NOT NULL,
-  `patient_id` int(11) NOT NULL,
-  `med_name` varchar(100) NOT NULL,
-  `med_qty` int(11) NOT NULL,
-  `dosage` int(11) NOT NULL,
-  `cost` int(11) NOT NULL,
-  `comments` text NOT NULL,
-  `md_date` date NOT NULL,
-  `status` varchar(5) NOT NULL DEFAULT '0'
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `medication`
---
-
-INSERT INTO `medication` (`id`, `patient_id`, `med_name`, `med_qty`, `dosage`, `cost`, `comments`, `md_date`, `status`) VALUES
-(2, 2, 'Panadol', 10, 2, 0, '', '2018-07-15', '1'),
-(5, 2, 'Delpa', 5, 1, 0, '', '2018-07-16', '1'),
-(6, 2, 'Cofta', 10, 3, 0, '', '2018-07-16', '1'),
-(7, 3, 'Panadol', 20, 2, 0, 'moja mara 3', '2018-07-16', '1'),
-(8, 3, 'Action', 10, 2, 0, '2 mara 4', '2018-07-16', '1'),
-(9, 2, 'Panadol', 10, 2, 0, '2 kwa siku', '2018-07-17', '1'),
-(10, 2, 'Delpa', 5, 1, 0, '1 mara 3', '2018-07-17', '1'),
-(30, 29, 'Colgate', 1, 1, 2000, '1', '2018-07-18', '1'),
-(29, 29, 'Panadol', 20, 1, 1000, '1 ma 4', '2018-07-18', '1'),
-(28, 24, 'Cogate', 2, 1, 0, '1 per week', '2018-07-17', '1'),
-(27, 24, 'Panadol', 30, 2, 0, '2 mara 3', '2018-07-17', '1'),
-(43, 30, 'Panadol', 5, 2, 500, 'dsfs fsd sdfds sdfsd fsd', '2018-07-20', '1'),
-(42, 30, 'Panadol', 5, 1, 500, 'Â dfsefsd fsd sdf sdfs d', '2018-07-20', '1'),
-(44, 30, 'CoolMax', 6, 2, 60000, 'sdfsd fsd fsdf sfsd fsd s', '2018-07-20', '1'),
-(45, 31, 'Panadol', 12, 2, 1200, '2 mara 3', '2018-07-20', '1'),
-(46, 31, 'CoolMax', 6, 2, 60000, '2 mara 1', '2018-07-20', '1'),
-(47, 34, 'Panadol', 10, 2, 1000, 'ule 2 mara 3', '2018-07-20', '1'),
-(48, 34, 'CoolMax', 20, 3, 200000, '3 mara 3', '2018-07-20', '1'),
-(49, 34, 'Headaxe', 3, 4, 600, '6 mara 5 ', '2018-07-20', '1'),
-(50, 35, 'Panadol', 10, 2, 1000, 'jahus adnjsnajdsna', '2018-07-25', '1'),
-(51, 35, 'CoolMax', 2, 3, 20000, 'sjbdajs', '2018-07-25', '1'),
-(52, 35, 'Mettacofline', 34, 3, 40800, 'jabsdbaj', '2018-07-25', '1'),
-(53, 36, 'Panadol', 10, 2, 1000, 'This is comment 1', '2018-08-15', '1'),
-(54, 36, 'Action', 8, 3, 1600, 'This is comment 2', '2018-08-15', '1'),
-(55, 36, 'Headaxe', 5, 3, 1000, 'This is comment 3', '2018-08-15', '1'),
-(56, 34, 'Panadol', 5, 5, 500, 'dgdf dfg dfgd gdf gdfgÂ ', '2018-08-31', '1'),
-(57, 32, 'Panadol', 10, 2, 1000, 'coment 1', '2018-08-31', '1'),
-(58, 32, 'Cofta', 5, 4, 750, 'coment 2', '2018-08-31', '1'),
-(59, 32, 'Headaxe', 6, 3, 1200, 'coment 3', '2018-08-31', '1'),
-(60, 37, 'Panadol', 20, 2, 2000, 'comment 2', '2018-08-31', '1'),
-(61, 0, 'Panadol', 121, 0, 12100, 'e1231', '2018-09-26', '0'),
-(62, 0, 'Mettacofline', 1231, 31, 1477200, 'xzx', '2018-09-26', '0'),
-(63, 39, 'Cofta', 4, 4, 600, 'thus sus usisiÂ ', '2019-02-27', '1'),
-(64, 39, 'Action', 3, 2, 600, 'thasu asua ia', '2019-02-27', '1'),
-(65, 40, 'CoolMax', 20, 6, 200000, 'hhhhhj', '2019-03-28', '1'),
-(66, 40, 'Mettacofline', 10, 7, 12000, 'hhhh', '2019-03-28', '1'),
-(67, 40, 'Cofta', 5, 5, 750, 'ggghgd', '2019-03-28', '1'),
-(68, 41, 'Panadol', 5, 2, 500, 'some comments', '2019-03-31', '1'),
-(69, 41, 'Mettacofline', 5, 2, 6000, 'some comments', '2019-03-31', '1');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `medicine`
---
-
-CREATE TABLE `medicine` (
-  `id` int(11) NOT NULL,
-  `m_name` varchar(100) NOT NULL,
-  `p_price` int(11) NOT NULL,
-  `s_price` int(11) NOT NULL,
-  `qty` int(11) NOT NULL,
-  `added_by` varchar(100) NOT NULL,
-  `added_at` date NOT NULL,
-  `status` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `medicine`
---
-
-INSERT INTO `medicine` (`id`, `m_name`, `p_price`, `s_price`, `qty`, `added_by`, `added_at`, `status`) VALUES
-(1, 'Panadol', 90, 100, 1000, 'Khadija Khamis', '2018-07-15', 'active'),
-(2, 'CoolMax', 9000, 10000, 90, 'Khadija Khamis', '2018-07-15', 'active'),
-(3, 'Mettacofline', 1000, 1200, 100, 'Khadija Khamis', '2018-07-15', 'active'),
-(4, 'Cofta', 100, 150, 90, 'Khadija Khamis', '2018-07-15', 'active'),
-(5, 'Action', 180, 200, 200, 'Khadija Khamis', '2018-07-15', 'active'),
-(6, 'Headaxe', 140, 200, 120, 'Khadija Khamis', '2018-07-15', 'active'),
-(7, 'Colgate', 1800, 2000, 100, 'Khadija Khamis', '2018-07-15', 'active'),
-(8, 'Whitedent', 2500, 3000, 150, 'Khadija Khamis', '2018-07-15', 'active');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `patient`
---
 
 CREATE TABLE `patient` (
   `patient_id` int(11) NOT NULL,
@@ -322,17 +187,9 @@ CREATE TABLE `patient` (
 --
 
 INSERT INTO `patient` (`patient_id`, `fname`, `lname`, `status`, `regNumber`, `address`, `phone`, `email`, `dob`, `gender`, `regDate`, `code`, `treated`) VALUES
-(2, 'Aisha', 'Deo', 'non student', '-', 'Kibonde mzungu', '773545566', 'abass@hotmail.com', '1995-05-15', 'male', '2018-05-15', 15271, 1),
-(37, 'Abdi', 'Kassim', 'student', '17/BFA/002', 'Kwahani', '0777543212', 'babi@gmail.com', '1999-06-17', 'male', '2018-08-31', 0, 1),
-(38, 'Ismail', 'Abdallah', 'student', '15/OIA/032', 'Kizimbani Wete Pemba', '0776667778', 'suma@gmail.com', '1995-12-30', 'male', '2018-11-07', 0, 0),
-(39, 'Amria', 'Ali', 'student', 'BFAA/2/18/001', 'Mikocheni', '0717676430', '-', '2013-02-01', 'female', '2019-02-27', 0, 1),
-(40, 'Lee', 'Sao', 'student', '15/BIA/090', 'Kizimbani', '0717676431', 'lee@gmail.com', '2012-05-14', 'male', '2019-03-28', 0, 1),
-(41, 'Ester', 'Kombo', 'student', 'BITA/123/2019', 'Kwahani', '0678578950', 'ester@gmail.com', '2008-03-05', 'female', '2019-03-31', 0, 1),
-(24, 'Fatma', 'Rashid', 'non student', '-', 'Mikocheni', '0776565643', 'fatma@hotmail.com', '1999-05-30', 'female', '2018-07-25', 0, 1),
-(29, 'Abass', 'Hassan', 'non student', '17/OIA/001', 'Kibonde mzungu', '773545566', 'abass@hotmail.com', '1997-05-30', 'male', '2018-07-16', 0, 1),
-(32, 'Jamila', 'Rajab', 'student', '17/OFA/008', 'Bububu', '0717676431', 'jamila@gmail.com', '1996-01-04', 'female', '2018-07-16', 0, 1),
-(34, 'Hussein', 'Khamis', 'student', '16/OIA/012', 'Kizimbani', '0711787872', 'hussein@gmail.com', '1973-06-13', 'male', '2018-07-20', 0, 1),
-(35, 'Juma', 'Ali', 'non student', '-', 'Kizimbani', '0777876540', 'juma@gmail.com', '1995-11-05', 'male', '2018-07-25', 0, 1);
+(2, 'Abhinav', 'Pundir', 'non student', '-', 'Jalandhar', '773545566', 'ab@hotmail.com', '1995-05-15', 'male', '2018-05-15', 15271, 1),
+(37, 'Tushar', 'Agg', 'student', '17/BFA/002', 'UP', '0777543212', 'tushar@gmail.com', '1999-06-17', 'male', '2018-08-31', 0, 1),
+(38, 'Isha', 'Singh', 'student', '15/OIA/032', 'Mumbai', '0776667778', 'isha@gmail.com', '1995-12-30', 'female', '2018-11-07', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -391,38 +248,6 @@ CREATE TABLE `sick_description` (
 --
 -- Dumping data for table `sick_description`
 --
-
-INSERT INTO `sick_description` (`id`, `patient_id`, `problem`, `since`, `nature`, `checked_by`, `checked_date`, `comment`) VALUES
-(4, 27, 'Typhoid', '2018-07-03', 'Sijui', 'Salum Rajab', '2018-07-07', 'This is some comment'),
-(5, 7, 'Kichwa', '2018-07-18', 'Sijui', 'Salum Bonano', '2018-07-15', 'Some comments'),
-(6, 7, 'Typhoid', '2018-07-19', 'Sijui', 'Salum Bonano', '2018-07-15', 'This is some comments'),
-(7, 32, 'Malaria', '2018-07-14', 'Kulala bila net', 'Salum Bonano', '2018-07-16', 'this is some doctor comment'),
-(8, 29, 'Homa', '2018-07-25', 'Sijui', 'Salum Bonano', '2018-07-16', 'Comment'),
-(9, 31, 'Kichwa', '2018-07-16', 'Sijui', 'Salum Bonano', '2018-07-17', 'Utumiaji mwingi wa kio'),
-(10, 2, 'Kichwa', '2018-07-16', 'Sijui', 'Salum Bonano', '2018-07-17', 'Comment comment'),
-(11, 24, 'Typhoid', '2018-07-11', 'Kulala bila net', 'Salum Bonano', '2018-07-17', 'some comments'),
-(12, 29, 'Tumbo', '2018-07-17', 'Sijui', 'Salum Bonano', '2018-07-18', 'some commentcsasa asda aahap hapa'),
-(14, 30, 'Homa', '2018-07-19', 'Kulala bila net', 'Marya Doe', '2018-07-20', 'asdas sadsad asdasd asdasd asdsadsa sads af a fafa fsa'),
-(15, 31, 'Homa', '2018-07-19', 'I dont know', 'Marya Doe', '2018-07-20', 'This is some comment about this patient'),
-(16, 33, 'Homa', '2018-07-19', 'I dont know', 'Marya Doe', '2018-07-20', 'gs fsdfsd'),
-(17, 34, 'Homa', '2018-07-19', 'I dont know', 'Marya Doe', '2018-07-20', 'This is some comment to this patient'),
-(18, 35, 'Homa', '2018-07-24', 'Sijui', 'Fatma  Sahim', '2018-07-25', 'haksd akdsaknak '),
-(22, 34, 'Homa', '2018-08-04', 'Sijui', 'Marya Doe', '2018-08-30', 'huhuhuhu'),
-(24, 34, 'Homa', '2018-08-11', 'Kulala bila net', 'Marya Doe', '2018-08-30', 'decgvgv vgvrft'),
-(25, 34, 'Homa', '2018-08-01', 'Sijui', 'Marya Doe', '2018-08-31', 'this is some comments'),
-(26, 32, 'Homa', '2018-08-09', 'Kulala bila net', 'Abuu Juma', '2018-08-31', 'this is is is'),
-(27, 37, 'Homa', '2018-08-30', 'Sijui', 'Fatma  Sahim', '2018-08-31', 'hey thusadsc ascas'),
-(28, 24, 'Kichwa', '2018-12-05', 'Sijui', 'Abuu Juma', '2018-12-05', 'test'),
-(29, 39, 'Kichwa', '2019-02-25', 'Sijui', 'Fahad Juma', '2019-02-27', 'this is doctors comment'),
-(30, 40, 'Homa', '2019-03-27', 'Sijui', 'Fatma  Sahim', '2019-03-28', 'homessad '),
-(31, 41, 'Fever', '2019-03-29', 'dont know', 'Abuu Juma', '2019-03-31', 'some comments');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
 CREATE TABLE `users` (
   `user_id` int(11) NOT NULL,
   `fname` varchar(25) NOT NULL,
